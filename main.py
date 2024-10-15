54cda7838a7653cf3daf37d3a2df35668ffef582
pyyas-star <visayasin@gmail.com>
2024-10-15 20:28:16 +0300

    data ingestion

---
+++ main.py
@@ -0,0 +1,14 @@
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier import logger
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
